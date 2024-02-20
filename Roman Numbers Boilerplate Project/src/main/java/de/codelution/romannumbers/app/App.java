608507cde17f8bfe97976fee01429adaf4e834a4
Stefan Dirschnabel <stefan.dirschnabel@codelution.de>
2024-02-20 15:29:34 +0100

    Roman Numbers Boilerplate Project

---
+++ Roman Numbers Boilerplate Project/src/main/java/de/codelution/romannumbers/app/App.java
@@ -0,0 +1,10 @@
+package de.codelution.romannumbers.app;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+       
+    }
+}
---
+++ Roman Numbers Boilerplate Project/src/test/java/de/codelution/romannumbers/SomeTestClass1.java
@@ -0,0 +1,18 @@
+package de.codelution.romannumbers;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import de.codelution.junit.jupiter.extensions.CodelutionTestWatcher;
+
+
+@ExtendWith(CodelutionTestWatcher.class) // we need to extend every test class like this
+public class SomeTestClass1 {
+
+    @Test
+    public void Some_Example_Test() {
+        assertTrue(false);
+    }
+}
